11b9b7495858390b9992c79faac37656bc1b1e3b
hyvi <tanhaiwei0222@gmail.com>
2019-01-25 20:57:02 +0800

    cancel in context by omitting event

---
+++ context/canceled-emitting-event.go
@@ -0,0 +1,40 @@
+// Emitting a cancellation event
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+func operation1() error {
+	//  sleep
+	time.Sleep(10 * time.Second)
+	return errors.New("failed")
+}
+
+func operation2(ctx context.Context) {
+	select {
+	case <-time.After(50 * time.Second):
+		fmt.Println("finished")
+	case <-ctx.Done():
+		fmt.Println("interupt")
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	// 如果operation1失败了，则operation2退出执行
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		err := operation1()
+		if err != nil {
+			cancel()
+		}
+	}()
+
+	fmt.Println("go context")
+	operation2(ctx)
+}
