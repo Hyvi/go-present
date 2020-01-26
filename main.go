ec2d14c1ee6d2c3d9a6b715fb55c09cd0295053a
Hyvi <tanhaiwei0222@gmail.com>
2020-01-26 16:07:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
+
+var unexp = errors.New("some unexported error") // MATCH /error var unexp should have name of the form errFoo/
 
 func main() {
 	fmt.Println("Hello, playground")
