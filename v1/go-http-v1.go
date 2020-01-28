5f1ad542f68dd529ec95178d47e0b9f73164fd94
Hyvi <tanhaiwei0222@gmail.com>
2020-01-28 23:00:15 +0800

    Update go-http-v1.go

--- v1/go-http-v1.go
+++ v1/go-http-v1.go
@@ -14,7 +14,8 @@ func main() {
 		w.Write([]byte("hello, version 1"))
 	})
 	
-	fmt.Sprintf("%d")
+	// 用于golangci github action 测试， 
+	// fmt.Sprintf("%d", 1) 
 
 	log.Println("start http server ...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
