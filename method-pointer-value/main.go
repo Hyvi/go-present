5059848c74c72dc27b9f214d0f6199c524e99b9e
Hyvi <tanhaiwei0222@gmail.com>
2022-03-20 13:58:16 +0000

    *T vs T Method sets

---
+++ method-pointer-value/main.go
@@ -0,0 +1,37 @@
+package main
+import (
+	"fmt"
+)
+
+type T struct {
+	s string	
+}
+ 
+// https://gronskiy.com/posts/2020-04-golang-pointer-vs-value-methods/#fn:1
+// type T struct { },  the copying will not happen and all the four addresses will be the same! 
+// Pointer type receiver
+func (receiver *T) pointerMethod() {
+    fmt.Printf("Pointer method called on \t%#v with address %p\n\n", *receiver, receiver)
+}
+
+// Value type receiver
+func (receiver T) valueMethod() {
+    fmt.Printf("Value method called on \t%#v with address %p\n\n", receiver, &receiver)
+}
+func main() {
+	var (
+		val     T  = T{}
+		pointer *T = &val
+	)
+
+	fmt.Printf("Value created \t%#v with address %p\n", val, &val)
+	fmt.Printf("Pointer created on \t%#v with address %p\n", *pointer, pointer)
+
+	val.valueMethod()
+	pointer.pointerMethod()	
+
+	// origin article wrong output
+
+	pointer.valueMethod()   // Implicitly converted to: (*pointer).valueMethod()
+	val.pointerMethod()   // Implicitly converted to: (&value).pointerMethod()
+}
